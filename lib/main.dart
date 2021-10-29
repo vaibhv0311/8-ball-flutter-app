6ce83d13ed94d53711ae412fe005c51c693f5b9a
vaibhav0311 <sharma.vaibhav0311@gmail.com>
2021-10-29 23:07:35 +0530

    Intial Upload

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[500],
+        appBar: AppBar(
+          title: Text("Eight Ball App"),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: MyApp(),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var num = 1;
+  void generate() {
+    num = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  print("Button got pressed!");
+                  setState(() {
+                    generate();
+                  });
+                },
+                child: Image(
+                  image: AssetImage('images/ball$num.png'),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
